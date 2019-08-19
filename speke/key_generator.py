bbe1f0b63fffb6497514230b158c812ea2af358d
Marko Martonosi <marko.martonosi@mediamonks.com>
2019-08-19 17:10:18 +0200

    Implement local filesystem caching

--- speke/key_generator.py
+++ speke/key_generator.py
@@ -2,6 +2,8 @@
 
 # import boto3
 # from botocore.exceptions import ClientError
+from oss2.exceptions import NoSuchKey
+import os
 
 
 class KeyGenerator:
@@ -12,108 +14,101 @@ class KeyGenerator:
     """
 
     def __init__(self):
-        pass
-        # TODO: After implementation of caching
-        # self.local_secret_folder = "/home/alibaba_speke_testing/"
+        self.local_secret_directory = "/tmp/.speke/secrets"
+        if not os.path.exists(self.local_secret_directory):
+            os.makedirs(self.local_secret_directory)
 
-    # Related to caching
-    # def local_secret_path(self, content_id):
-    #     """
-    #     Create a path for a content ID secret file stored locally in the Lambda filesystem
-    #     """
-    #     return f"{self.local_secret_folder}/speke.{content_id}"
+    def local_secret_path(self, content_id):
+        """
+        Create a path for a content ID secret file stored locally in the Lambda filesystem
+        """
+        return f"{self.local_secret_directory}/{content_id}"
 
-    # Related to caching
-    # def store_local_secret(self, content_id, secret):
-    #     """
-    #     Store a content ID secret file
-    #     """
-    #     secret_file = self.local_secret_path(content_id)
-    #     secret_file = open(secret_file, 'w')
-    #     secret_file.write(secret)
-    #     secret_file.close()
+    def store_local_secret(self, content_id, secret):
+        """
+        Store a content ID secret file
+        """
+        secret_file = self.local_secret_path(content_id)
+        secret_file = open(secret_file, 'w')
+        secret_file.write(secret)
+        secret_file.close()
 
-    # Related to acching
-    # def retrieve_local_secret(self, content_id):
-    #     """
-    #     Retrieve a content ID secret file
-    #     """
-    #     secret_file = self.local_secret_path(content_id)
-    #     secret_file = open(secret_file, 'r')
-    #     secret = secret_file.read()
-    #     secret_file.close()
-    #     return secret
+    def retrieve_local_secret(self, content_id):
+        """
+        Retrieve a content ID secret file
+        """
+        secret_file = self.local_secret_path(content_id)
+        secret_file = open(secret_file, 'r')
+        secret = secret_file.read()
+        secret_file.close()
+        return secret
 
     def retrieve_content_id_secret(self, content_id):
         """
         Retrieve the secret value by content ID used for generating keys
         """
+        # TODO: fix this late import
+        from services.oss import acs_oss_get_secret, acs_oss_create_secret
 
-        from services.oss import \
-            acs_oss_get_secret  # TODO: fix this late import
+        try:
+            # cached locally?
+            secret = self.retrieve_local_secret(content_id)
+            print("GOT SECRET FROM CACHE")
+            print(f"SECRET: {content_id}")
+            print(f"SECRET: {secret}")
+        except (IOError, FileNotFoundError):
+            # try oss
+            try:
+                secret = acs_oss_get_secret(content_id)
+                self.store_local_secret(content_id, secret)
+                print("GOT SECRET FROM OSS")
+                print(f"SECRET: {content_id}")
+                print(f"SECRET: {secret}")
+            except NoSuchKey:
+                # create new content and salt for it
+                # new secret value
+                # TODO: move this to config of the class
+                content_id_secret_length = 64
+                # generate random string "password"
+                secret = generate_password(content_id_secret_length)
 
-        # TODO: implement caching
-        print("SECRET FROM OSS")
-        secret = acs_oss_get_secret(content_id)
-        # try:
-        #     # cached locally?
-        #     secret = self.retrieve_local_secret(content_id)
-        #     print("GOT SECRET FROM CACHE")
-        #     print(f"SECRET: {content_id}")
-        # except IOError:
-        #     # try secrets manager (oss in alibaba case)
-        #     # secret_id = f"speke/{content_id}"
-        #     try:
-        #         # response = self.secrets_client.get_secret_value(SecretId=secret_id)
-        #         secret = acs_oss_get_secret(content_id)
-        #     # oos get key by secret_id(path - id of rhb)
-        #     # secret = response['SecretString']
-        #     # self.store_local_secret(content_id, secret)
-        #     # oss.store_secret()
-        #         print("GOT SECRET FROM SECRETS MANAGER")
-        #         print(f"SECRET: {content_id}")
-        #     except ClientError as error:
-        #         if error.response['Error']['Code'] == 'ResourceNotFoundException':
-        #             # new secret value
-        #             content_id_secret_length = 64
-        #             secret = self.secrets_client.get_random_password(
-        #                          PasswordLength=content_id_secret_length)['RandomPassword']
-        #         # we need a new secret value
-        #
-        #         # set length
-        #         content_id_secret_length = 64
-        #
-        #         print("CREATE-SECRET {}".format(content_id))
-        #         secret = self.secrets_client.get_random_password(
-        #             PasswordLength=content_id_secret_length)['RandomPassword']
-        #         self.secrets_client.create_secret(Name=secret_id,
-        #                                           SecretString=secret,
-        #                                           Description='SPEKE content ID secret value for key generation')
-        #         self.store_local_secret(content_id, secret)
-        #     else:
-        #         # we're done trying
-        #         raise error
+                # store on oss and in local cache
+                acs_oss_create_secret(content_id, secret)
+
+                self.store_local_secret(content_id, secret)
+                print(f"CREATED SECRET {content_id}")
+                print(f"SECRET {content_id}")
+                print(f"SECRET: {secret}")
         return secret
 
-    def key(self, content_id, key_id):
-        """
-        Return a symmetric key based on a content ID and key ID
-        """
-        # TODO: fix all late imports
-        from cryptography.hazmat.primitives import hashes
-        from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-        from cryptography.hazmat.backends import default_backend
 
-        # Setup for key derivation function (default)
-        backend = default_backend()
-        derived_key_iterations = 5000
-        derived_key_size = 16
-        # Generate a key using a key derivation function (default)
+def key(self, content_id, key_id):
+    """
+    Return a symmetric key based on a content ID and key ID
+    """
+    # TODO: fix late imports
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    from cryptography.hazmat.backends import default_backend
 
-        salt = self.retrieve_content_id_secret(content_id)
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
-                         length=derived_key_size,
-                         salt=salt,
-                         iterations=derived_key_iterations,
-                         backend=backend)
-        return kdf.derive(key_id.encode('utf-8'))
+    # Setup for key derivation function (default)
+    backend = default_backend()
+    derived_key_iterations = 5000
+    derived_key_size = 16
+    # Generate a key using a key derivation function (default)
+
+    salt = self.retrieve_content_id_secret(content_id)
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+                     length=derived_key_size,
+                     salt=salt,
+                     iterations=derived_key_iterations,
+                     backend=backend)
+    return kdf.derive(key_id.encode('utf-8'))
+
+
+def generate_password(length):
+    # https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
+    import secrets  # TODO: FIX LATE IMPORT
+    import string  # TODO: FIX LATE IMPORT
+    alphabet = string.ascii_letters + string.digits
+    return "".join(secrets.choice(alphabet) for i in range(length))
--- speke/services/oss.py
+++ speke/services/oss.py
@@ -1,17 +1,17 @@
 import io
+import os
 
 import oss2
-import os
 
 auth = oss2.Auth(os.environ["ACCESS_KEY_ID"], os.environ["ACCESS_SECRET"])
 bucket = oss2.Bucket(auth, os.environ["OSS_ENDPOINT"], os.environ["OSS_BUCKET_NAME"])
 
+
 def acs_oss_create_secret(content_id, secret):
-    text_stream = io.BytesIO(secret)
-    # Store secret of a content in a folder/secret-like struture
-    bucket.put_object(f"{content_id}/secret.txt", text_stream.read())
+    # text_stream = io.BytesIO(secret)
+    bucket.put_object(f"speke/{content_id}", secret)
 
 
 def acs_oss_get_secret(content_id):
-    cipher_text = bucket.get_object(f"{content_id}/secret.txt")
-    return cipher_text.read()
+    cipher_text = bucket.get_object(f"speke/{content_id}")
+    return cipher_text.read().decode()
